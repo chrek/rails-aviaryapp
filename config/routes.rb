b832da446aad319b0b42cd3f03ca744e8c0c50af
chrek <chrek@ymail.com>
2019-10-22 17:10:08 +0200

    changed root url

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :posts
   end
 
-  root 'aviaries#index'
+  root 'welcome#index'
+
+  # root 'aviaries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
