18f8494bac8e9f748e954a9069c2975e49a1e90a
chrek <chrek@ymail.com>
2019-10-22 23:00:47 +0200

    using credentials

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
     resources :posts
   end
 
-  root 'welcome#index'
-
-  # root 'aviaries#index'
+  root 'aviaries#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
