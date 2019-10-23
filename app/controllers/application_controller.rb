b4862edaa557922a353f8a289cf9bd41ef39a2f9
chrek <chrek@ymail.com>
2019-10-23 11:34:47 +0200

    added http_basic_authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # http_basic_authenticate_with name: 'aviaryuser', password: 'aviarypswd', 
-  # except: [:index, :show]
+  http_basic_authenticate_with name: 'aviaryuser', password: 'aviarypswd', 
+  except: [:index, :show]
   # only: [:create, :update, :destroy]
 end
